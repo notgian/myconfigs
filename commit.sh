d8cf242c40da2fbd7326b14e468ecf80810742d8
notgian <85060753+notgian@users.noreply.github.com>
2025-10-28 13:53:53 +0800

    Adding interactive commit script
    This script also allows to add a description to the commit.

---
+++ commit.sh
@@ -0,0 +1,53 @@
+# This script provides an interactive way to commit changes for this repositoryA
+
+echo "Adding files to commit..."
+git add .
+
+echo ""
+echo "Enter commit message (leave blank for default message): "
+message="Updating my config files"
+read inp
+
+if [[ ! -z $inp ]]; then
+    message=$inp
+fi
+
+echo ""
+echo "Enter commit description (leave blank for no description)"
+description=""
+read inp
+
+if [[ ! -z $inp ]]; then
+    description=$inp
+fi
+
+echo ""
+echo "Making new commit..."
+
+# Making the commit
+if [[ -z $description ]]; then # no description
+    git commit -m $message
+else # with description
+    git commit -m "$message" -m "$description"
+fi
+
+echo ""
+echo "Finished creating commit!"
+
+looping=true
+echo "Push to github? [y/n]"
+while [[ $looping = true ]]; do
+    read -r -n 1 response
+    case "$response" in
+        y | Y) 
+            looping=false
+            git push 
+            ;;
+        n | N )
+            looping=false
+            ;;
+        *) 
+            ;;
+    esac
+done
+
--- get-files.nu
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env nu
-
-# assumption that this is run with sudo
-cp -r $"($env.HOME)/.config/nvim" ./nvim
-cp $"($env.HOME)/.tmux.conf" ./tmux/.tmux.conf
-cp $"($env.HOME)/.config/nushell/config.nu" ./nushell/config.nu
-
