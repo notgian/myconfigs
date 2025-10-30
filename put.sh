3a0778d8aac5b618938f0dcc9b1cf2640c1d0a89
notgian <85060753+notgian@users.noreply.github.com>
2025-10-30 21:17:50 +0800

    added put script
    the put script allows the user to interactively put files into the directories. It uses y/n prompts to ensure no unwanted changes are made

--- put.sh
+++ put.sh
@@ -1,29 +1,102 @@
 # This script can be used to interactively put the configs in the folders.
-# Take note that this script must be updated if any new configs are added.
 
 source ./parse_configs.sh
 
-# local_folders=()
-# config_defaults=()
-# config_default_location="$HOME/.config"
-# declare -A config_files_custom
-
+# The function that puts all the configs that belong to the default
+# location, typically at ~/.config
 function put_defaults() {
-    echo "Putting configs that belong in the default config location ($config_default_location)"
+    echo -e "Putting configs that belong in the default config location \033[0m(\033[36m$config_default_location\033[0m)"
     cwd=${ pwd; }
 
     [[ ! -d ".backups" ]] && mkdir ".backups"
 
     for name in "$@"; do 
         if [[ -d $name ]]; then
-            echo "backing up $name config..."
-            
-            echo "- copying $name config to $config_default_location/$name"
-            cp -r "$name" "$default_config_location"
+            echo -e "\033[33mPreparing to put $name config\033[0m"
+            echo -e "- backing up \033[36m $config_default_location/$name\033[0m"
+            cp -r $config_default_location/$name ./.backups/
+
+            echo -e "- copying $name config to \033[36m$config_default_location"
+            echo -e "  \033[33mIs this correct? [y/n]"
+
+            valid_inp=false
+            correct=true
+            until [[ $valid_inp == true ]]; do
+                read -n 1 inp
+                [[ $inp == "Y" || $inp == "y" ]] && valid_inp=true && correct=true
+                [[ $inp == "N" || $inp == "n" ]] && valid_inp=true && correct=false
+            done
+
+            echo ""
+
+            [[ $correct == false ]] && echo -e "\033[0mSkipping putting $name config...\n" && continue
+    
+            echo -e "\033[37m"
+            echo "$config_default_location"
+            cp -r -v "$name" "$config_default_location/"
+            echo -e "\033[0m\033[1j"
         else
-            echo "- WARNING: local folder $cwd/$name cannot be found. Skipping this config..."
+            echo "-\033[31mWARNING: local folder $cwd/$name cannot be found. Skipping this config...\033[0m"
         fi
     done
 }
 
+function put_others() {
+    echo -e "\033[0mPutting configs with special file locations..."
+    local -n config_files_ref=$1 
+
+    for key in ${!config_files_ref[@]}; do
+        files=${ ls -a $key; }
+
+        for put_file in ${files[@]}; do
+            if [[ $put_file != "." && $put_file != ".." ]]; then
+                IFS_TEMP=$IFS
+                IFS=";"
+                
+                echo -e "\033[33mInferring put destination of $put_file..."
+
+                file_dest=""
+                for file_item in ${config_files_custom[$key]}; do
+                    
+                    match=${ echo $file_item | grep $put_file ; }
+                    
+                    [[ ! -z $match ]] && file_dest=$match
+                done
+
+                echo -e "\033[0m- Found $put_file destination: \033[36m$file_dest"
+                echo -e "  \033[33mIs this correct? [y/n]"
+
+                valid_inp=false
+                correct=true
+                until [[ $valid_inp == true ]]; do
+                    read -n 1 inp
+                    [[ $inp == "Y" || $inp == "y" ]] && valid_inp=true && correct=true
+                    [[ $inp == "N" || $inp == "n" ]] && valid_inp=true && correct=false
+                done
+                
+                IFS=$IFS_TEMP
+                echo ""
+
+                if [[ $correct == false ]]; then
+                    echo -e "\033[0mSkipping putting $put_file..."
+                    continue
+                fi
+
+                echo -e "- \033[0mcopying \033[36m$key/$put_file \033[0mto \033[36m$file_dest\033[0m..." 
+                echo -e "\033[37m"
+                cp -v $key/$put_file $file_dest
+            fi
+        done
+    done
+}
+
+
+if [ "$EUID" -ne 0 ]; then 
+    echo "This script needs elevated permissions. Please run it as root (sudo ./put.sh)"
+    exit
+fi
+
 put_defaults ${config_defaults[@]}
+put_others config_files_custom
+
+echo -e "\n\033[92;1mPutting configs complete!\033[0m"
